435e4863fe997000edefb55ce8592f4b6cd1d3b2
lijk <78848256+kirass1@users.noreply.github.com>
2025-06-15 16:37:02 +0800

    Add sequence embedding LBM model

--- src/lbm/models/lbm/__init__.py
+++ src/lbm/models/lbm/__init__.py
@@ -1,4 +1,5 @@
 from .lbm_config import LBMConfig
 from .lbm_model import LBMModel
+from .lbm_vector import SequenceLBMConfig, SequenceLBMModel
 
-__all__ = ["LBMModel", "LBMConfig"]
+__all__ = ["LBMModel", "LBMConfig", "SequenceLBMModel", "SequenceLBMConfig"]
